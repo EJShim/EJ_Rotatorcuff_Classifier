55c351d562d1ac00994e617dcdac4e7d79082ef0
soulrommel <soulrommel@gmail.com>
2017-09-11 15:50:13 +0900

    update

--- test.py
+++ test.py
@@ -1,5 +1,44 @@
-string =  "Ha_Woa_AWEg"
+import gdcm
+import mudicom
 
-print(string)
+path = "/Users/EJ/Desktop/ser004img00008.dcm"
 
-print(string.lower().find('woa'))
+reader = gdcm.Reader()
+reader.SetFileName(path)
+if not reader.Read():
+    raise InvalidDicom("Not a valid DICOM file")
+
+
+_file = reader.GetFile()
+_header = _file.GetHeader()
+_dataset = _file.GetDataSet()
+_str_filter = gdcm.StringFilter()
+
+
+print(_header)
+print(_dataset)
+
+
+# mu = mudicom.load(path)
+
+# # returns array of data elements as dicts
+# mu.read()
+
+
+# # returns dict of errors and warnings for DICOM
+# mu.validate()
+
+# # basic anonymization
+# mu.anonymize()
+# # save anonymization
+# mu.save_as("dicom.dcm")
+
+# # creates image object
+# img = mu.image # before v0.1.0 this was mu.image()
+# # returns numpy array
+# img.numpy # before v0.1.0 this was mu.numpy()
+
+# # using Pillow, saves DICOM image
+# img.save_as_pil("ex1.jpg")
+# # using matplotlib, saves DICOM image
+# img.save_as_plt("ex1_2.jpg")
