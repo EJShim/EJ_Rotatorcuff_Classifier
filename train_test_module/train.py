a856373552773c78e0257ac692d473f6d3d2ef35
EJ Shim <soulrommel@gmail.com>
2017-11-20 09:47:52 +0900

    update

--- train_test_module/train.py
+++ train_test_module/train.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 #Add Root Dir
 file_path = os.path.dirname(os.path.realpath(__file__))
-root_path = os.path.abspath(os.path.join(file_path, os.pard))
+root_path = os.path.abspath(os.path.join(file_path, os.pardir))
 sys.path.insert(0, root_path)
 # sys.setrecursionlimit(2000)
 
@@ -67,7 +67,7 @@ cam_data = []
 
 #Training Functions
 learning_rate = tf.placeholder(tf.float32)
-loss_op = tf.reduce_mean(tf.nn.sparse_sigmoid_cross_entropy_with_logits(logits=y, labels=y_true))
+loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=y_true))
 regularizer = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables() if 'bias' not in v.name])#
 loss = loss_op + 0.001*regularizer
 trainer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9, use_nesterov=True)
@@ -112,7 +112,7 @@ for epoch in range(max_epochs):
     loss_ax.plot(loss_data, 'r-')
     plt.pause(1e-45)
     #Save ROC Data
-    np.savez_compressed(os.path.join(file_path, os.pardir, 'roc_data', 'tf_epoch' + str(epoch)), y=test_targets, score=score)
+    np.savez_compressed(os.path.join(file_path,  'roc_data', 'tf_epoch' + str(epoch)), y=test_targets, score=score)
     np.savez_compressed(os.path.join(file_path, 'train_record'), loss=loss_data, accuracy=acc_data, cam=cam_data)
 
     #Training Module
