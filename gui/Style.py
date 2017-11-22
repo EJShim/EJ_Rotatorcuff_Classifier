0b95d8d15ee66642995bd63170d46a800c428ffc
ejshim <soulrommel@gmail.com>
2017-11-22 13:39:42 +0900

    update

---
+++ gui/Style.py
@@ -0,0 +1,52 @@
+styleData="""
+QWidget
+{
+    color: #b1b1b1;
+    background-color: #323232;
+}
+QListView { 
+    alternate-background-color: #434343;
+}
+QTreeView { 
+    alternate-background-color: #434343;
+}
+QProgressBar
+{
+    border: 2px solid grey;
+    border-radius: 5px;
+    text-align: center;
+}
+QProgressBar::chunk
+{
+    background-color: #1a80d7;
+    width: 2.15px;
+    margin: 0.5px;
+}
+
+QTabWidget::tab-bar {
+    left: 5px;
+}
+
+
+QTabBar::tab {
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    min-width: 8ex;
+    padding: 2px;
+}
+
+QHeaderView::section
+{
+    background-color: #76797C;
+    color: #eff0f1;
+    padding: 5px;
+    border: 1px solid #FF0000;
+}
+
+QToolBar {
+    border: 1px transparent #FF0000;
+    background: 1px solid #323232;
+    font-weight: bold;
+}
+
+"""
